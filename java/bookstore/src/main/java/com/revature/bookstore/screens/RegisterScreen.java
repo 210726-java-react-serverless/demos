c26286f73c6fcda9168fff1125aee6a14cbf9eb6
Teldu <spsmithsj@gmail.com>
2021-07-29 14:47:39 -0700

    made id random instead of iterative

--- java/bookstore/src/main/java/com/revature/bookstore/screens/RegisterScreen.java
+++ java/bookstore/src/main/java/com/revature/bookstore/screens/RegisterScreen.java
@@ -6,9 +6,11 @@ import com.revature.bookstore.services.UserService;
 import com.revature.bookstore.util.ScreenRouter;
 
 import java.io.BufferedReader;
+import java.util.Random;
 
 public class RegisterScreen extends Screen {
 
+    private Random rand = new Random();
     private final UserService userService;
     private int idVal;
 
@@ -36,7 +38,7 @@ public class RegisterScreen extends Screen {
         System.out.print("Password: ");
         String password = consoleReader.readLine();
 
-        idVal++;
+        idVal = rand.nextInt(1000);
         AppUser newUser = new AppUser(firstName, lastName, email, username, password, idVal);
 
         System.out.println(newUser);
