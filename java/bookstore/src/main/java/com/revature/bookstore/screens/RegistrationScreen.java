405fb1c4b23690fd24edbd0496ba31b3d8549d7b
jdsmith1906 <jdsmith1906@gmail.com>
2021-07-28 21:37:27 -0400

    added registration

---
+++ java/bookstore/src/main/java/com/revature/bookstore/screens/RegistrationScreen.java
@@ -0,0 +1,12 @@
+package com.revature.bookstore.screens;
+
+public class RegistrationScreen extends Screen{
+    public RegistrationScreen(){
+        super("Registration","Register");
+    }
+    
+    @Override
+    public void render() {
+
+    }
+}
