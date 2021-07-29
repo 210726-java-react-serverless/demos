4d72a771c0730cc05dd491ac80a0dd27fc6ba7a1
jdsmith1906 <jdsmith1906@gmail.com>
2021-07-29 10:04:19 -0400

    added if logic to try/catch blocks

--- java/bookstore/src/main/java/com/revature/bookstore/screens/Person.java
+++ java/bookstore/src/main/java/com/revature/bookstore/screens/Person.java
@@ -10,43 +10,67 @@ public class Person {
 
 
     public static void Render() throws IOException {
+        String lastname;
+        String firstname;
+        String username;
+        String password;
+
         System.out.println("Lets get you registered");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("Enter your last name: ");
         try {
-            String lastname = reader.readLine();
-        } catch (IOException ioe){
-            System.out.println("Exception raised");
-            //commented out StackTraces because of lengthy outputs
-            //ioe.printStackTrace();
+             lastname = reader.readLine();
+        } catch (IOException ioe) {
+            if (lastname == null) {
+                System.out.println("Exception raised");
+                //commented out StackTraces because of lengthy outputs
+                //ioe.printStackTrace();
+            }if (lastname != null){
+                reader.close();
+            }
         }
 
         System.out.println("Enter your first name: ");
         try {
-            String firstname = reader.readLine();
-        } catch (IOException ioe){
-            System.out.println("Exception raised");
-            //ioe.printStackTrace();
+             firstname = reader.readLine();
+        }
+        if (firstname == null) {
+        catch(IOException ioe){
+                System.out.println("Exception raised");
+                //ioe.printStackTrace();
+            }if (firstname != null){
+            reader.close();
+            }
         }
 
         System.out.println("Enter your user name: ");
         try {
             String username = reader.readLine();
-        } catch (IOException ioe){
-            System.out.println("Exception raised");
-            //ioe.printStackTrace();
+        }
+
+        catch (IOException ioe) {
+            if (username == null) {
+                System.out.println("Exception raised");
+                //ioe.printStackTrace();
+            }if (username != null){
+                reader.close();
+            }
         }
 
         System.out.println("Enter your password: ");
         try {
             String password = reader.readLine();
-        } catch (IOException ioe){
-            System.out.println("Exception raised");
-            //ioe.printStackTrace();
+        } catch (IOException ioe) {
+            if (password == null) {
+                System.out.println("Exception raised");
+                //ioe.printStackTrace();
+            } if (password != null){
+                reader.close();
+            }
         }
 
-        reader.close();
+        
 
 
 
