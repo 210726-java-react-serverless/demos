a9ecdcafb0bfa448458995b66a39d2ea42db0ee6
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-07-29 21:27:40 -0400

    added comments and a preamble.txt file

--- java/bookstore/src/main/java/com/revature/bookstore/repositories/UserRepository.java
+++ java/bookstore/src/main/java/com/revature/bookstore/repositories/UserRepository.java
@@ -7,18 +7,19 @@ import java.io.*;
 public class UserRepository implements CrudRepository<AppUser> {
 
     private File dataSource;
-
-    @Override
-    public AppUser findById(int id) {
-        return null;
-    }
-
-
-    public boolean findByName(String username, String password) {
+    /*
+    I put this method here instead of making it part of the CRUDREP interface because the logic is
+    specific to finding the username and password. I maybe could have parameterized it as a String varargs
+    to take any one of the AppUser fields, and use it more as a general search method.
+     */
+    public boolean authenticate(String username, String password) {
         dataSource = new File("src/main/resources/data.txt");
         try (
                 BufferedReader reader = new BufferedReader(new FileReader(dataSource));
         ) {
+            /*Each line of the data.txt file is split into a string array. Assuming that the order in which AppUser
+             fields are saved doesn't change, we know ahead of time where each field will be in the array.
+             */
             String line = reader.readLine();
             while (line != null) {
                 String[] splittedLine = line.split(":");
@@ -29,7 +30,12 @@ public class UserRepository implements CrudRepository<AppUser> {
             } return false;
         } catch (IOException e) {
             System.err.print("Exception thrown from file search.");
-        } return false;
+        } return false; //the method should never reach this return statement, but Intellij complained.
+    }
+
+    @Override
+    public AppUser findById(int id) {
+        return null;
     }
 
     @Override
--- java/bookstore/src/main/java/com/revature/bookstore/screens/LoginScreen.java
+++ java/bookstore/src/main/java/com/revature/bookstore/screens/LoginScreen.java
@@ -15,9 +15,12 @@ public class LoginScreen extends Screen {
 
     @Override
     public void render() throws Exception {
-        UserRepository repo = new UserRepository();
+        UserRepository repo = new UserRepository(); //see the authenticate() method in the UserRepo class definition
         try{
             BufferedReader stream  = new BufferedReader(new InputStreamReader(System.in));
+            /* User inputs username and password, both are passed to the authenticate method. Passed in an
+             unending while loop that only breaks if you enter valid credentials, or you type 'exit' for the username.
+             */
             while(true) {
                 System.out.print("Enter your username, or type 'exit' to exit: ");
                 String username = stream.readLine();
@@ -25,20 +28,24 @@ public class LoginScreen extends Screen {
                     break;
                 System.out.print("Enter your password: ");
                 String password = stream.readLine();
-                if (repo.findByName(username, password)) {
+                if (repo.authenticate(username, password)) {
                     System.out.println("Login successful!");
                     break;
                 } else{
                     System.out.println("Invalid username or password. Please try again, or type 'exit'");
-
                 }
 
             }
                 router.navigate("/welcome");
             } finally{
-
+                            /*this finally block needed to keep Intellij happy. Can't close this stream or else the
+                            while loop in startup() will loop forever. The Welcome Screen won't have an open stream to
+                            read from, so an exception is thrown up to the AppState.startup() method, which will print
+                            the stack trace and continue the while loop, running into the same error over and over again.
+                             */
             }
 
     }
+
 
 }
