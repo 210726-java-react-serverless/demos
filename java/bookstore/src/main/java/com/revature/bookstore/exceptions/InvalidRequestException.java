44af2fa648294d6375037a22ce4f19bb0ac96c89
heraldOfMechanus <jccallahan11@gmail.com>
2021-07-30 09:31:41 -0500

    Repaired the scanner. Everything works as it should. Hooray for Heather and I!

--- java/bookstore/src/main/java/com/revature/bookstore/exceptions/InvalidRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.revature.bookstore.exceptions;
-
-public class InvalidRequestException extends RuntimeException {
-
-    public InvalidRequestException(String message) {
-        super(message);
-    }
-
-}
