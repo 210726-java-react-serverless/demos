16640f31a6edb05f61b4b48dfe7194521ef4580a
raneyj <raney.jack99@gmail.com>
2021-07-31 20:13:25 -0500

    Forgot to push this exception with last commit

---
+++ bookstore/bookstore/src/main/java/com/revature/bookstore/exceptions/InvalidRouteException.java
@@ -0,0 +1,8 @@
+package com.revature.bookstore.exceptions;
+
+public class InvalidRouteException extends RuntimeException {
+    public InvalidRouteException(String message) {
+        super(message);
+    }
+
+}
