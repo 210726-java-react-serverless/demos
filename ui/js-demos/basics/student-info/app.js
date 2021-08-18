eafb44db3e17ca5f01b05360f3cf4e5e0f3faee9
wsingleton <wezley.singleton@gmail.com>
2021-08-18 13:12:50 -0400

    set activities up for after lunch

---
+++ ui/js-demos/basics/student-info/app.js
@@ -0,0 +1,9 @@
+/*
+
+    Write some JS logic that will allow you to enter some student information into the form fields on the page.
+    When the "Add Student" button is clicked, insert the provided data into the table body. Be sure to generate
+    an auto-incrementing ID value for each student added to the table.
+
+    NOTE: You will not need to edit the HTML file at all for this activity.
+
+ */
