a6e7e66b411071559f1f1aa36913b927f1650453
LunaHaines <nhaines95@gmail.com>
2021-08-18 12:38:27 -0600

    finished student-info excercise

--- ui/js-demos/basics/student-info/app.js
+++ ui/js-demos/basics/student-info/app.js
@@ -7,3 +7,37 @@
     NOTE: You will not need to edit the HTML file at all for this activity.
 
  */
+
+const studentName = document.getElementById("student-name");
+const major = document.getElementById("student-major");
+const button = document.getElementById("add-student");
+const tableBody = document.getElementById("student-table-body");
+
+button.addEventListener('click', addStudent);
+major.addEventListener('keydown', function(e) {
+    if (e.key == 'Enter') {
+        addStudent();
+    }
+})
+
+let id = 0;
+
+function addStudent() {
+    // add student to table
+    let newStudent = `
+    <tr>
+        <td>
+            ${id}
+        </td>
+        <td>
+            ${studentName.value}
+        </td>
+        <td>
+            ${major.value}
+        </td>
+    </tr>`;
+    tableBody.innerHTML += newStudent;
+    id++;
+    studentName.value = '';
+    major.value = '';
+}
