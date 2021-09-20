2b565a89acdb90b8b79efbea76c440db4b4767df
wsingleton <wezley.singleton@gmail.com>
2021-09-20 16:06:57 -0400

    update serverless demos

---
+++ serverless/poc-java-lambda/src/main/java/com/revature/poc_lambda/Address.java
@@ -0,0 +1,14 @@
+package com.revature.poc_lambda;
+
+import lombok.Data;
+
+@Data
+public class Address {
+
+    private String unitNumber;
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+
+}
